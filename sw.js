45634a6793755e3abe92acf2b2fcb6254833fb27
Rishikesh <rishi2025k@gmail.com>
2026-08-31 23:30:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,40 @@
-const CACHE = "gate-tracker-v1";
+const CACHE = "gate-tracker-v3";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.svg"
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon.svg"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+    event.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
